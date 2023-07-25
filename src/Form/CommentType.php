98c9bcf4a475d26790e94e494ab7d8988cceb258
Istinnie <n.istinnie.velonkasina@gmail.com>
2023-07-25 18:56:14 +0400

    style des formulaires

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,13 +6,24 @@ use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('message')
+            ->add('message', TextType::class,array(
+                
+                'label'=> 'Message',
+                'attr' => array(
+                    'class' => 'form-control mb-3'
+                )
+            ))
             ->add('rating')
             // ->add('createdAt')
             // ->add('restaurant')
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,13 +11,21 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class,array(
+                'label'=> 'Email',
+                'attr' => array(
+                    'class' => 'form-control mb-3'
+                )
+            ))
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -25,12 +33,16 @@ class RegistrationFormType extends AbstractType
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+                'label'=> 'Accepter les termes',
+                'attr' => array(
+                    'class' => 'ml-3 mb-5'
+                )
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password','class'=>'form-control mb-3'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
